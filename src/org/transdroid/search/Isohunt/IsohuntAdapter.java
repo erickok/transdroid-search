398d7a046fe7a24de03d2aa91756077c153c15bc
erickok <erickok@54971fc8-50e7-5d66-a234-ca7faaa4fd5c>
2010-10-15 22:26:54 +0000

    Added correct copyright/license text.
    git-svn-id: https://transdroid-search.googlecode.com/svn/trunk@9 54971fc8-50e7-5d66-a234-ca7faaa4fd5c

--- src/org/transdroid/search/Isohunt/IsohuntAdapter.java
+++ src/org/transdroid/search/Isohunt/IsohuntAdapter.java
@@ -2,19 +2,19 @@
  *	This file is part of Transdroid Torrent Search 
  *	<http://code.google.com/p/transdroid-search/>
  *	
- *	Transdroid is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ *	Transdroid Torrent Search is free software: you can redistribute 
+ *	it and/or modify it under the terms of the GNU Lesser General 
+ *	Public License as published by the Free Software Foundation, 
+ *	either version 3 of the License, or (at your option) any later 
+ *	version.
  *	
- *	Transdroid is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *	Transdroid Torrent Search is distributed in the hope that it will 
+ *	be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *	See the GNU Lesser General Public License for more details.
  *	
- *	You should have received a copy of the GNU General Public License
- *	along with Transdroid.  If not, see <http://www.gnu.org/licenses/>.
- *	
+ *	You should have received a copy of the GNU Lesser General Public 
+ *	License along with Transdroid.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.transdroid.search.Isohunt;
 
--- src/org/transdroid/search/RssFeedSearch/RssFeedSearchAdapter.java
+++ src/org/transdroid/search/RssFeedSearch/RssFeedSearchAdapter.java
@@ -2,19 +2,19 @@
  *	This file is part of Transdroid Torrent Search 
  *	<http://code.google.com/p/transdroid-search/>
  *	
- *	Transdroid is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ *	Transdroid Torrent Search is free software: you can redistribute 
+ *	it and/or modify it under the terms of the GNU Lesser General 
+ *	Public License as published by the Free Software Foundation, 
+ *	either version 3 of the License, or (at your option) any later 
+ *	version.
  *	
- *	Transdroid is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *	Transdroid Torrent Search is distributed in the hope that it will 
+ *	be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *	See the GNU Lesser General Public License for more details.
  *	
- *	You should have received a copy of the GNU General Public License
- *	along with Transdroid.  If not, see <http://www.gnu.org/licenses/>.
- *	
+ *	You should have received a copy of the GNU Lesser General Public 
+ *	License along with Transdroid.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.transdroid.search.RssFeedSearch;
 
@@ -26,8 +26,6 @@ import org.ifies.android.sax.RssParser;
 import org.transdroid.search.ISearchAdapter;
 import org.transdroid.search.SearchResult;
 import org.transdroid.search.SortOrder;
-
-import android.util.Log;
 
 /**
  * An abstract class providing functionality to easily build search engine support for torrent sites that use RSS feeds.
