8bc4c4397cf1f3dbff36b3d1b52a2bd2836b3c5c
Eric Kok <erickok@gmail.com>
2018-10-08 20:56:54 +0200

    Change limetorrents domain to new tld. Cf #88.

--- app/src/main/java/org/transdroid/search/adapters/rss/publictrackers/LimeTorrentsAdapter.java
+++ app/src/main/java/org/transdroid/search/adapters/rss/publictrackers/LimeTorrentsAdapter.java
@@ -52,7 +52,7 @@ public class LimeTorrentsAdapter extends RssFeedSearchAdapter {
 	@Override
 	protected String getUrl(String query, SortOrder order) {
 		try {
-			return "http://www.limetorrents.com/searchrss/" + URLEncoder.encode(query, "UTF-8") + "/";
+			return "http://www.limetorrents.co/searchrss/" + URLEncoder.encode(query, "UTF-8") + "/";
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 			return null;
